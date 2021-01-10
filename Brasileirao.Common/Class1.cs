e1348f1ad233dee9a8c0e081e4a390f78aed3ffb
Weliton Silva <Weliton.Mesquita.Silva@formandos.cinel.pt>
2021-01-10 12:18:04 +0000

    Init Project

---
+++ Brasileirao.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Brasileirao.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Brasileirao.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Brasileirao.UIClassic
+{
+    public class Class1
+    {
+    }
+}

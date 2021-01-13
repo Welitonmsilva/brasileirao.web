a25c0d645b8e5a5fd32711ad46ef778ad5f4b30d
Weliton Silva <Weliton.Mesquita.Silva@formandos.cinel.pt>
2021-01-13 00:03:51 +0000

    update-database Creat controllers

---
+++ Brasileirao.web/Migrations/20210112232447_brasileirao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Brasileirao.web.Migrations
+{
+    public partial class brasileirao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jogos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Jornadas = table.Column<string>(nullable: true),
+                    Clube = table.Column<string>(nullable: true),
+                    Pontos = table.Column<int>(nullable: false),
+                    Posicao = table.Column<int>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    ultimoJogo = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogos");
+        }
+    }
+}
